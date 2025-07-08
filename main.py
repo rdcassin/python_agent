2d60db06c961e10887aa9d66eadf283ef48195db
rdcassin <rdcassin@gmail.com>
2025-07-08 12:54:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    print("Hello from python-agent!")
+
+
+if __name__ == "__main__":
+    main()
